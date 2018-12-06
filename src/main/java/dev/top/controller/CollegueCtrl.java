eea310cf9585a7750965f410f4a0589dfa17a4c7
Champetier <todave11@gmail.com>
2018-12-06 17:37:59 +0100

    créé collegue

--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -1,16 +1,22 @@
 package dev.top.controller;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import dev.top.entities.ActionButton;
 import dev.top.entities.Avis;
@@ -19,40 +25,66 @@ import dev.top.entities.Vote;
 import dev.top.repos.CollegueRepo;
 import dev.top.repos.VoteRepo;
 
-
 @CrossOrigin
 @RestController()
 @RequestMapping("/collegues")
 public class CollegueCtrl {
 
-    @Autowired
-    private CollegueRepo collegueRepo;
-    
-    @Autowired
-    private VoteRepo voteRepo;
+	@Autowired
+	private CollegueRepo collegueRepo;
 
-    @GetMapping
-    public List<Collegue> findAll() {
-        return this.collegueRepo.findAll();
-    }
-    
-    @GetMapping("/{pseudo}")
-    public Collegue findCollegueDetail(@PathVariable String pseudo) {
-        return collegueRepo.findByPseudo(pseudo);
-    }
-    
-    @PatchMapping("/{pseudo}")
-    public Vote updateScore(@PathVariable String pseudo,@RequestBody ActionButton action) {
-    	Collegue col = collegueRepo.findByPseudo(pseudo);
-    		System.out.println(col);
-    		if (action.getAction()==Avis.AIMER) {
-    			col.setScore(col.getScore()+10);
-			}else {
-				col.setScore(col.getScore()-5);
+	@Autowired
+	private VoteRepo voteRepo;
+
+	@GetMapping
+	public List<Collegue> findAll() {
+		return this.collegueRepo.findAll();
+	}
+
+	@GetMapping("/{pseudo}")
+	public Collegue findCollegueDetail(@PathVariable String pseudo) {
+		return collegueRepo.findByPseudo(pseudo);
+	}
+
+	@PatchMapping("/{pseudo}")
+	public Vote updateScore(@PathVariable String pseudo, @RequestBody ActionButton action) {
+		Collegue col = collegueRepo.findByPseudo(pseudo);
+		System.out.println(col);
+		if (action.getAction() == Avis.AIMER) {
+			col.setScore(col.getScore() + 10);
+		} else {
+			col.setScore(col.getScore() - 5);
+		}
+		collegueRepo.save(col);
+		Vote myVote = new Vote(col, action.getAction(), col.getScore(), LocalDateTime.now());
+		voteRepo.save(myVote);
+		return myVote;
+	}
+
+	@PostMapping("/nouveau")
+	public ResponseEntity create(@RequestBody Map<String, String> form) {
+		RestTemplate rt = new RestTemplate();
+		Collegue[] listCollegue = rt.getForObject(
+				"http://collegues-api.cleverapps.io/collegues?matricule=" + form.get("matricule"), Collegue[].class);
+		if (listCollegue.length > 0) {
+
+			Collegue collegue = new Collegue();
+			collegue.setNom(listCollegue[0].getNom());
+			collegue.setPrenom(listCollegue[0].getPrenom());
+			collegue.setEmail(listCollegue[0].getEmail());
+			collegue.setAdresse(listCollegue[0].getAdresse());
+			collegue.setPseudo(form.get("pseudo"));
+			if (form.get("pseudo") != null) {
+				String[] listImg = { form.get("pseudo") };
+				collegue.setImageUrl(Arrays.asList(listImg));
+			} else {
+				collegue.setImageUrl(listCollegue[0].getImageUrl());
 			}
-    		collegueRepo.save(col);
-    		Vote myVote=new Vote(col,action.getAction(),col.getScore(),LocalDateTime.now());
-    		voteRepo.save(myVote);
-    		return myVote;
-    }
+			collegueRepo.save(collegue);
+			return ResponseEntity.status(HttpStatus.OK).body("request sucess");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad matricule");
+		}
+	}
+
 }
